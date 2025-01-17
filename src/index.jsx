9d1544e139e7316277f49654dfc6bba8ad0b7453
Mitesh-V-Chauhan <miteshchauhan1843@gmail.com>
2025-01-17 22:58:45 +0530

    Added Correct Title

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"; // Make sure App.jsx is in the same folder
-import { BrowserRouter as Router } from 'react-router-dom'; // If using React Router
-import "./index.css"; // Optional: If you have a global CSS file
+import App from "./App"; 
+import { BrowserRouter as Router } from 'react-router-dom'; 
+import "./index.css"; 
 
 // Render the app to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/My-Portfolio"> {/* If using React Router */}
+    <Router basename="/My-Portfolio"> 
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById("root") // Ensure there's a div with id="root" in your index.html
+  document.getElementById("root") 
 );
