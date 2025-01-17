113713d3238d4ef52fd1e281849feb2c096325d6
Mitesh-V-Chauhan <miteshchauhan1843@gmail.com>
2025-01-17 23:22:43 +0530

    Fixed React rendering issue and updated routing

--- src/index.jsx
+++ src/index.jsx
@@ -13,3 +13,21 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root") 
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";  // Make sure to use `react-dom/client` for React 18+
+// import App from "./App";
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import "./index.css"; 
+
+// // Create the root element
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// // Render the app to the DOM
+// root.render(
+//   <React.StrictMode>
+//     <Router basename="/My-Portfolio"> 
+//       <App />
+//     </Router>
+//   </React.StrictMode>
+// );
