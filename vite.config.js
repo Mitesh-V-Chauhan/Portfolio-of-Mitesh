cfcbd561fdee9f7f66f89b8d7f59ce073147b170
Mitesh-V-Chauhan <miteshchauhan1843@gmail.com>
2025-01-18 13:25:55 +0530

    First commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/', // Ensure this matches your GitHub repository name
-  build: {
-    outDir: 'build', // The build directory that will be used for deployment
-  },
-});
+})
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/My-Portfolio/', // Ensure this matches your GitHub repository name
+//   build: {
+//     outDir: 'build', // The build directory that will be used for deployment
+//   },
+// });
